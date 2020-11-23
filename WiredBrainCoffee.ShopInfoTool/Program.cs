c5117ca4a4f341dd673611894dc0e0d84b559d5b
Ven <srinivav@yahoo.com>
2020-11-23 17:14:25 -0500

    Add project files.

---
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using WiredBrainCoffee.DataAccess;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+              "write 'quit' to exit application");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
+
+        }
+    }
+}
+
